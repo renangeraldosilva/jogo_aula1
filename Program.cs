187c4f0f2d2085ac89dabbe1890c537360d279e1
renangeraldosilva <renangeraldosilva@gmail.com>
2022-04-12 17:44:16 -0300

    jogo para acertar numero randomico - Cast Group

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Aula1jogo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float sorteiro = new Random().Next(-10, 8);
+            Console.WriteLine("digite um numero: ");
+            float numeroUsuario = float.Parse(Console.ReadLine());
+            int tentativas = 0;
+            while (tentativas < 5)
+            {
+                if (numeroUsuario == sorteiro)
+                {
+                    Console.WriteLine("você acertou o numero é: " + sorteiro);
+                    tentativas = 5;
+                }
+                else
+                {
+                    Console.WriteLine($"voce errou o numero era: {sorteiro}");
+                    tentativas++;
+                    Console.WriteLine("{0} de 5 chances. ", tentativas);
+
+                    sorteiro = new Random().Next(-10, 8);
+                    Console.WriteLine("digite um numero: ");
+                    numeroUsuario = float.Parse(Console.ReadLine());
+                }
+                
+            }
+            
+        }
+    }
+}
